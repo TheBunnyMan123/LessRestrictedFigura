56e834ac009688ef1dce673fe58099f5068c26ec
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-13 22:02:17 -0500

    Disable sandbox

--- common/src/main/java/com/thekillerbunny/lessrestrictions/GlobalsAccess.java
+++
@@ -1,9 +0,0 @@
-package com.thekillerbunny.lessrestrictions;
-
-import org.luaj.vm2.LuaTable;
-
-public interface GlobalsAccess {
-    public LuaTable lessRestrictedFigura$getBuiltInDebugLib();
-
-    public void lessRestrictedFigura$setBuiltInDebugLib(LuaTable table);
-}
--- common/src/main/java/com/thekillerbunny/lessrestrictions/mixin/FiguraLuaRuntimeMixin.java
+++ common/src/main/java/com/thekillerbunny/lessrestrictions/mixin/FiguraLuaRuntimeMixin.java
@@ -4,12 +4,12 @@ import org.figuramc.figura.lua.FiguraLuaRuntime;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = FiguraLuaRuntime.class, remap = false)
 public class FiguraLuaRuntimeMixin {
-    @Inject(method = "setupFiguraSandbox()V", at = @At("HEAD"), cancellable = true)
-    public void setupFiguraSandboxInjection(CallbackInfoReturnable<Void> ci) {
+    @Inject(method = "setupFiguraSandbox", at = @At("HEAD"), cancellable = true)
+    public void setupFiguraSandboxInjection(CallbackInfo ci) {
         ci.cancel();
     }
 }
